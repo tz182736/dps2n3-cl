4fe4b88ecb612812c4ad0c969a38aa54cc82c1ef
tz182736 <tz182736@gmail.com>
2023-05-14 21:38:57 +0630

    redo app vs main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { styled, ThemeProvider } from '@mui/material/styles';
 
 import './index.css'
-import theme from './theme.tsx';
 
 import { App as Home } from './pages/main-entry.tsx';
 import Bet_type from './pages/bet-type-config.tsx';
@@ -13,28 +11,29 @@ import Sale_carry_lucky from './pages/sale-carry-lucky.tsx';
 import Sale_summary from './pages/sale-summary.tsx';
 import NotFound from './pages/NotFound.tsx';
 
+import { CustomerListProps } from "./shared/customer";
+
 import DpsAppBar from './components/DpsAppBar.tsx';
 import Tmp from './pages/tmp-page.tsx';
 
 const Offset = styled('div')(({ theme: Theme }) => Theme.mixins.toolbar);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Router>
-        <DpsAppBar />
-        <Offset /> <br />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path="/bet-type-config" element={<Bet_type />} />
-          <Route path="/customer-config" element={<Customer_config />} />
-          <Route path="/sale-carry-lucky" element={<Sale_carry_lucky />} />
-          <Route path="/sale-summary" element={<Sale_summary />} />
-          <Route element={<NotFound />} />
-          <Route path="/tmp" element={<Tmp />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
-    ,
-  </React.StrictMode>,
-)
+
+export const App = () => {
+  
+  return (<>
+    <Router>
+      <DpsAppBar />
+      <Offset /> <br />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/bet-type-config" element={<Bet_type />} />
+        <Route path="/customer-config" element={<Customer_config {CustomerListProps} />} />
+        <Route path="/sale-carry-lucky" element={<Sale_carry_lucky />} />
+        <Route path="/sale-summary" element={<Sale_summary />} />
+        <Route element={<NotFound />} />
+        <Route path="/tmp" element={<Tmp />} />
+      </Routes>
+    </Router>
+  </>);
+}
