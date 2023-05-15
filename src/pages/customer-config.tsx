b7404ae33ce653bcbcca71444caa83955508e9b5
tz182736 <tz182736@gmail.com>
2023-05-15 07:55:22 +0630

    update customer

--- src/pages/customer-config.tsx
+++ src/pages/customer-config.tsx
@@ -1,136 +1,71 @@
-// I’m not sure what you are trying to do, but I can try to help you with your code. Here is a possible React TypeScript CRUD UI component for your Customer interface:
+import React, { useState, useEffect } from "react";
+import { Customer } from "../shared/customer"; // assuming the interface is defined in a separate file
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from "@mui/material";
 
-import React, { useState } from "react";
-import { Customer, CustomerListProps } from "../shared/customer";
+// assuming there is an API endpoint that returns an array of customers
+const API_URL = "https://example.com/api/customers";
 
-interface CustomerProps {
-    customer: Customer;
-    onUpdate: (customer: Customer) => void;
-    onDelete: (id: number) => void;
-}
+export const CustomerPage: React.FC = () => {
+  // use state to store the customers array
+  const [customers, setCustomers] = useState<Customer[]>([]);
 
-const CustomerItem: React.FC<CustomerProps> = ({
-    customer,
-    onUpdate,
-    onDelete,
-}) => {
-    const [editMode, setEditMode] = useState(false);
-    const [editedCustomer, setEditedCustomer] = useState<Customer>(customer);
+  // use effect to fetch the data from the API when the component mounts
+  useEffect(() => {
+    // define an async function to fetch the data
+    const fetchData = async () => {
+      try {
+        // make a GET request to the API and parse the response as JSON
+        const response = await fetch(API_URL);
+        const data = await response.json();
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setEditedCustomer({ ...editedCustomer, [name]: value });
+        // update the state with the data
+        setCustomers(data);
+      } catch (error) {
+        // handle any errors
+        console.error(error);
+      }
     };
 
-    const handleUpdate = () => {
-        onUpdate(editedCustomer);
-        setEditMode(false);
-    };
+    // call the function
+    fetchData();
+  }, []); // pass an empty dependency array to run the effect only once
 
-    const handleDelete = () => {
-        onDelete(customer.Id);
-    };
-
-    return (
-        <div className="customer-item">
-            {editMode ? (
-                <div className="customer-edit">
-                    <input
-                        type="text"
-                        name="Name"
-                        value={editedCustomer.Name}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="text"
-                        name="Phone"
-                        value={editedCustomer.Phone}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="number"
-                        name="BetTotal"
-                        value={editedCustomer.BetTotal}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="number"
-                        name="WinTotal"
-                        value={editedCustomer.WinTotal}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="number"
-                        name="Balance"
-                        value={editedCustomer.Balance}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="number"
-                        name="Rate2D"
-                        value={editedCustomer.Rate2D}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="number"
-                        name="Commission2D"
-                        value={editedCustomer.Commission2D}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="number"
-                        name="Rate3D"
-                        value={editedCustomer.Rate3D}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="number"
-                        name="Commission3D"
-                        value={editedCustomer.Commission3D}
-                        onChange={handleChange}
-                    />
-                    <button onClick={handleUpdate}>Save</button>
-                    <button onClick={() => setEditMode(false)}>Cancel</button>
-                </div>
-            ) : (
-                <div className="customer-view">
-                    <p>{customer.Name}</p>
-                    <p>{customer.Phone}</p>
-                    <p>{customer.BetTotal}</p>
-                    <p>{customer.WinTotal}</p>
-                    <p>{customer.Balance}</p>
-                    <p>{customer.Rate2D}</p>
-                    <p>{customer.Commission2D}</p>
-                    <p>{customer.Rate3D}</p>
-                    <p>{customer.Commission3D}</p>
-                    <button onClick={() => setEditMode(true)}>Edit</button>
-                    <button onClick={handleDelete}>Delete</button>
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell>BetTotal</TableCell>
+            <TableCell>WinTotal</TableCell>
+            <TableCell>Balance</TableCell>
+            <TableCell>Rate2D</TableCell>
+            <TableCell>Commission2D</TableCell>
+            <TableCell>Rate3D</TableCell>
+            <TableCell>Commission3D</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((customer) => (
+            <TableRow key={customer.Id}>
+              <TableCell>{customer.Id}</TableCell>
+              <TableCell>{customer.Name}</TableCell>
+              <TableCell>{customer.Phone}</TableCell>
+              <TableCell>{customer.BetTotal}</TableCell>
+              <TableCell>{customer.WinTotal}</TableCell>
+              <TableCell>{customer.Balance}</TableCell>
+              <TableCell>{customer.Rate2D}</TableCell>
+              <TableCell>{customer.Commission2D}</TableCell>
+              <TableCell>{customer.Rate3D}</TableCell>
+              <TableCell>{customer.Commission3D}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
-
-
-const CustomerList: React.FC<CustomerListProps> = ({
-    customers,
-    onUpdate,
-    onDelete,
-}) => {
-    return (
-        <div className="customer-list">
-            <h1>{customers?.length} </h1>
-            {customers?.map((customer) => (
-                <CustomerItem
-                    key={customer.Id}
-                    customer={customer}
-                    onUpdate={onUpdate}
-                    onDelete={onDelete}
-                />
-            ))}
-        </div>
-    );
-};
-
-export default CustomerList;
+export default CustomerPage;
