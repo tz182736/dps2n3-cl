ee569d8b26985deb724c36fb2fd56c1b0dc2f538
tz182736 <tz182736@gmail.com>
2023-05-17 03:14:31 +0630

    table vs collection sample on tmp-page

--- src/pages/test-only/tmp-page.tsx
+++ src/pages/test-only/tmp-page.tsx
@@ -1,96 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { Button, TextField } from '@mui/material';
 import Dexie from 'dexie';
 
-// Define the interface for the data type
-export interface BetTypeConfig {
-    CurrentBetType: '2D' | '3D';
-    Rate2D: number;
-    Commission2D: number;
-    Rate3D: number;
-    Commission3D: number;
-}
-
-// Create a Dexie database with a table for BetTypeConfig
-const db = new Dexie('BetTypeConfigDB');
+// Create a Dexie database with a table for people
+const db = new Dexie('PeopleDB');
 db.version(1).stores({
-    betTypeConfigs: 'CurrentBetType',
+    people: 'id,name,email',
 });
 
-// Create a React component that uses the interface and the database
-export default function BetTypeConfigForm() {
-    // Use a state variable to store the config data
-    const [config, setConfig] = useState<BetTypeConfig>({
-        CurrentBetType: '2D',
-        Rate2D: 0,
-        Commission2D: 0,
-        Rate3D: 0,
-        Commission3D: 0,
+// Use a table to add a new person to the object store
+db.table('people')
+    .add({ id: 1, name: 'Alice', email: 'alice@example.com' })
+    .then(() => {
+        console.log('Added Alice to people');
     });
 
-    // Handle input changes and update the config state
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
-        setConfig({
-            ...config,
-            [name]: value,
-        });
-    };
+// Use a table to update an existing person in the object store
+db.table('people')
+    .update(1, { email: 'alice@new.com' })
+    .then(() => {
+        console.log(`Updated Alice's email`);
+    });
 
-    // Handle form submission and save the config to IndexedDB
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // Use Dexie to put the config data in the table
-        db.table('betTypeConfigs')
-            .put(config)
-            .then(() => {
-                console.log('Saved config to IndexedDB');
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    };
+// Use a table to delete a person from the object store
+db.table('people')
+    .delete(1)
+    .then(() => {
+        console.log('Deleted Alice from people');
+    });
 
-    // Render the form with Material UI components and compact style
-    return (
-        <form onSubmit={handleSubmit}>
-            <TextField
-                label="Current Bet Type"
-                name="CurrentBetType"
-                value={config.CurrentBetType}
-                onChange={handleChange}
-                select
-                SelectProps={{
-                    native: true,
-                }}
-                size='small'
-                sx={{ width: '10rem', p: 1 }} // Use sx prop to apply compact style
-            >
-                <option value="2D">2D</option>
-                <option value="3D">3D</option>
-            </TextField><div />
-            <TextField
-                label="Rate 2D"
-                name="Rate2D"
-                type="number"
-                value={config.Rate2D}
-                onChange={handleChange}
-                size='small'
-                sx={{ width: '10rem', p: 1 }} // Use sx prop to apply compact style
-            />
-            <TextField
-                label="Commission 2D"
-                name="Commission2D"
-                type="number"
-                value={config.Commission2D}
-                onChange={handleChange}
-                size='small'
-                sx={{ width: '10rem', p: 1 }} // Use sx prop to apply compact style
-            />
-            <Button type="submit" variant="contained" color="primary"
-                size='small' sx={{ mt: 1.5 }}>
-                Update
-            </Button>
+/// collection is query
+// Use a collection to get all the people with name starting with A
+db.table('people')
+    .where('name')
+    .startsWith('A')
+    .toArray()
+    .then((people) => {
+        console.log('People with name starting with A:', people);
+    });
 
-        </form >);
-}
+// Use a collection to get the first person with name equal to Bob
+db.table('people')
+    .where('name')
+    .equals('Bob')
+    .first()
+    .then((person) => {
+        console.log('First person with name Bob:', person);
+    });
+
+// Use a collection to modify all the people with email ending with @example.com
+db.table('people')
+    // require index on property .where('email').endsWith('@example.com') 
+    .filter((person) => person.name.endsWith('e'))
+    .modify({ email: 'changed@example.com' })
+    .then(() => {
+        console.log('Modified email for people ending with @example.com');
+    });
